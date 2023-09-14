59b63e8858e03f5c80eb9ebffd148eba681d1af2
fred <fangqing.fan@hotmail.com>
2023-09-14 17:31:40 +0800

    upd form mbp2020

--- common/src/main/java/com/fc/common/socket/protobuf/proto2Dart.sh
+++ common/src/main/java/com/fc/common/socket/protobuf/proto2Dart.sh
@@ -10,9 +10,34 @@ rm -rf DART_TARGET_PATH/*.dart
 
 cd $PROTO_SRC_PATH
 
+PROTO_MSG_PATH=$DART_TARGET_PATH/proto_msg.dart
+rm $PROTO_MSG_PATH
+touch $PROTO_MSG_PATH
+
 for i in *.proto
 do
 echo "generate cli protocol dart code: $i"
 echo $DART_TARGET_PATH
 protoc --dart_out=$DART_TARGET_PATH ./$i
+done
+
+
+for i in *.proto
+do
+array=(${i//./ })
+name=${array[0]}
+echo "import '$name.pb.dart' as $name;" >> $PROTO_MSG_PATH
+done
+
+
+
+for i in *.proto
+do
+echo "" >> $PROTO_MSG_PATH
+array=(${i//./ })
+name=${array[0]}
+echo "typedef C2S_${name} = $name.C2S;" >> $PROTO_MSG_PATH
+echo "typedef C2SMsg_${name} = $name.C2SMsg;" >> $PROTO_MSG_PATH
+echo "typedef S2C_${name} = $name.S2C;" >> $PROTO_MSG_PATH
+echo "typedef S2CMsg_${name} = $name.S2CMsg;" >> $PROTO_MSG_PATH
 done
